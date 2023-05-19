185e55888f422c8eb39ca64d61a73c982488e21d
PiusEzekiel <e.pius1@alustudent.com>
2023-05-19 05:22:57 -0700

    1-repetition_token_0.rb

--- regular_expressions/1-repetition_token_0.rb
+++ regular_expressions/1-repetition_token_0.rb
@@ -4,8 +4,4 @@ input_string = ARGV[0] # Obtain the first command-line argument
 
 matches = input_string.match(/hbt{2,5}n/) # Find the first occurrence that matches the pattern
 
-if matches
-  puts "Na #{matches[0]} wey I see o!"
-else
-  puts "No match found o!"
-end
+puts matches
