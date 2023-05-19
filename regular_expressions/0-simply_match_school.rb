3c600df7296ebf2599124bcda54f9746f24267b9
PiusEzekiel <e.pius1@alustudent.com>
2023-05-19 05:16:42 -0700

    0-simply_match_school.rb

--- regular_expressions/0-simply_match_school.rb
+++ regular_expressions/0-simply_match_school.rb
@@ -1,9 +1,5 @@
 #!/usr/bin/env ruby
-
-input_string = ARGV[0] # Get the first command-line argument
-
+input_string = ARGV[0]
 matches = input_string.scan(/School/) # Find all occurrences of "School" in the input string
-
 result = matches.join # Concatenate the matches into a single string
-
 puts result # Output the result
